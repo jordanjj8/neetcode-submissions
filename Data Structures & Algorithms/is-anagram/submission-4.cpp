02cf16bb5f03e9bf7b3870c5781bb675a0a79f52
Jordan Justin Leung <jordanjj8@gmail.com>
2026-07-30 13:39:07 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // use a hashmap
+        // and compare the maps between the two
+        // time: o(n) one pass over s & t
+        // space: o(n) for n as number of unique characters
+        // if we have a fixed alphabe, then we can use a fixed array of size 26
+        vector<int> sCount(26, 0);
+        vector<int> tCount(26, 0);
+
+        for (char c: s) {
+            sCount[tolower(c) - 'a']++;
+        }
+        for (char c: t) {
+            tCount[tolower(c) - 'a']++;
+        }
+
+        return tCount == sCount;
+    }
+};
