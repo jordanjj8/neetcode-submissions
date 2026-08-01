22af6b0b7f45939f0a55145b07ee10c166e77741
Jordan Justin Leung <jordanjj8@gmail.com>
2026-08-01 14:03:01 -0700

    Add: observer - submission-2

---
+++ Design Patterns/observer/submission-2.cpp
@@ -0,0 +1,50 @@
+class Observer {
+public:
+    virtual void notify(string& itemName) = 0;
+};
+
+class Customer : public Observer {
+private:
+    string name;
+    int notifications;
+
+public:
+    Customer(string& name) : name(name), notifications(0) {}
+
+    void notify(string& itemName) override {
+        notifications += 1;
+    }
+
+    int countNotifications() {
+        return notifications;
+    }
+};
+
+class OnlineStoreItem {
+private:
+    string itemName;
+    int stock;
+    unordered_set<Observer*> subscribers;
+
+public:
+    OnlineStoreItem(string& itemName, int stock) : itemName(itemName), stock(stock) {}
+
+    void subscribe(Observer* observer) {
+        subscribers.insert(observer);
+    }
+
+    void unsubscribe(Observer* observer) {
+        subscribers.erase(observer);
+    }
+
+    void updateStock(int newStock) {
+        if (stock <= 0 && newStock > 0) {
+            // notify subscribers!
+            for (auto& observer: subscribers) {
+                observer->notify(itemName);
+            }
+        }
+        // update stock memeber
+        stock = newStock;
+    }
+};
