b09204e131274f213a8eab4db379d4cdda57843f
Jordan Justin Leung <jordanjj8@gmail.com>
2026-08-05 14:42:08 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       if (nums.size() < 1) {
+            return 0;
+       } 
+       if (nums.size() == 1) {
+            return nums[0];
+       }
+       if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+       }
+
+       vector<int> money(nums.size(), 0);
+       money[0] = nums[0];
+       money[1] = max(nums[0], nums[1]);
+
+
+       for (int i = 2; i < nums.size(); ++i) {
+            // don't rob or rob 
+            money[i] = max(money[i-1], nums[i] + money[i-2]);
+       }
+       return money[nums.size() -1];
+    }
+};
